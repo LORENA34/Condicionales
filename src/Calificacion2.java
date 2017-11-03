47dda3ab54f12871ae4f10a38996cab76cdbd85d
lorena salinas <nena_sr83@hotmail.com>
2017-11-03 19:28:53 -0400

    Condiciones utilizando el switch

---
+++ src/Calificacion2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB15
+ */
+public class Calificacion2 
+ 
+{
+    
+public static void main(String[] args) 
+
+  {
+  float nota;  
+  nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+   if(nota ==7)
+      {
+    JOptionPane.showInputDialog("NOTABLE");
+    
+    }
+    else
+      {
+   if(nota==6)   
+        { 
+      JOptionPane.showInputDialog("Bien");
+        }
+       else 
+       {
+      if(nota > 4&& nota < 5)  
+      {         
+       JOptionPane.showInputDialog("Siguiente");   
+         
+               }
+    
+       }   
+
