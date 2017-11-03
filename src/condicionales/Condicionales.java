f8ca2cae0cf7375b509c635a1596e8f3d26fefbd
lorena salinas <nena_sr83@hotmail.com>
2017-11-02 21:05:24 -0400

    el programa ejecuta el promedio del alumno

---
+++ src/condicionales/Condicionales.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author LAB15
+ */
+public class Condicionales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+    float numero1, numero2;
+    numero1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Primer Numero"));
+    numero2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese el Segundo Numero"));    
+
+ if (numero1 == numero2)//Aqui va una condicion
+ {
+
+//Si la condicion se cumple se ejecuta este codigo
+JOptionPane.showMessageDialog(null, "Son iguales");
+    }
+ else//Si no se cumple la condicion se ejecuta esto 
+ { 
+
+     JOptionPane.showMessageDialog(null, "Son Diferentes");
+if (numero1 > numero2)
+{
+    JOptionPane.showMessageDialog(null, "Numero"+numero1+"es mayor");   
+ 
+    }
+ else
+ {
+    JOptionPane.showMessageDialog(null, "Numero"+numero2+"es mayor");
+         }
+    
+  }
+    
